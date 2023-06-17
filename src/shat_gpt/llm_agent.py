988e6a18e2335665cc959ea4d3c9093d83fdc9af
Nicholas Kappel <nicholas.kappel@alexanderthamm.com>
2023-06-17 13:54:32 +0200

    weaviate client test

--- src/shat_gpt/llm_agent.py
+++ src/shat_gpt/llm_agent.py
@@ -1,9 +1,29 @@
+import os
+from dotenv import load_dotenv
 from typing import Optional
 from langchain.tools import BaseTool
 from langchain.callbacks.manager import (
     AsyncCallbackManagerForToolRun,
     CallbackManagerForToolRun,
 )
+from langchain.vectorstores import Weaviate
+import weaviate
+
+
+load_dotenv()
+
+WEAVIATE_URL = os.environ["WEAVIATE_URL"]
+WEAVIATE_API_KEY = os.environ["WEAVIATE_API_KEY"]
+# provide class name, e.g. 'LangChain_(...)'
+CLASS_NAME = "LangChain_35f237fe6ad54b7ea80682d55e5d28e4"  # 35f2... is class name of our dummy index
+auth_config = weaviate.AuthApiKey(api_key=WEAVIATE_API_KEY)
+
+# Instantiate the client with the auth config
+client = weaviate.Client(url=WEAVIATE_URL, auth_client_secret=auth_config)
+weaviate_instance = Weaviate(client=client, index_name=CLASS_NAME, text_key="text")
+
+print("weaviate", type(weaviate_instance))
+print("weaviate retriever", type(weaviate_instance.as_retriever()))
 
 
 class CustomSearchTool(BaseTool):
@@ -15,7 +35,7 @@ class CustomSearchTool(BaseTool):
     def _run(
         self, query: str, run_manager: Optional[CallbackManagerForToolRun] = None
     ) -> str:
-        store = None  # TODO: add vectorstore here
+        store = weaviate_instance.as_retriever()  # TODO: add vectorstore here
         docs = store.get_relevant_documents(query)
         text_list = [doc.page_content for doc in docs]
         return "\n".join(text_list)
