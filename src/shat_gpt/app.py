7a54feb29bd999b53f9608339b095c1cad542517
Nicholas Kappel <nicholas.kappel@alexanderthamm.com>
2023-06-17 16:42:15 +0200

    use RetrievalQAWithSourcesChain in frontend

--- src/shat_gpt/app.py
+++ src/shat_gpt/app.py
@@ -3,21 +3,35 @@ import chainlit as cl
 from dotenv import load_dotenv
 from langchain import PromptTemplate, OpenAI, LLMChain
 from langchain.agents import load_tools, initialize_agent, AgentType
+from langchain.chains import RetrievalQAWithSourcesChain
 from langchain.vectorstores import Weaviate
 from langchain.tools import BaseTool
 from llm_agent import CustomSearchTool
+import weaviate
+
 
 load_dotenv()
-API_KEY = os.environ.get("OPENAI_API_KEY")
+API_KEY = os.environ.get("COHERE_API_KEY")
 WEAVIATE_URL = os.environ["WEAVIATE_URL"]
+WEAVIATE_API_KEY = os.environ["WEAVIATE_API_KEY"]
+CLASS_NAME = "TextItem"  # TextItem is our best index
+auth_config = weaviate.AuthApiKey(api_key=WEAVIATE_API_KEY)
 
-llm = OpenAI(model_name="text-davinci-003", temperature=0)
-
-
-tools = [CustomSearchTool()]
-agent = initialize_agent(
-    tools, llm, agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION, verbose=True
+# Instantiate the client with the auth config
+client = weaviate.Client(
+    url=WEAVIATE_URL,
+    auth_client_secret=auth_config,
+    additional_headers={"X-Cohere-Api-Key": API_KEY},
 )
+
+weaviate_instance = Weaviate(
+    client=client, index_name=CLASS_NAME, text_key="text", attributes=["source"]
+)
+llm = OpenAI(model_name="text-davinci-003", temperature=0.0)
+# tools = [CustomSearchTool()]
+# agent = initialize_agent(
+#     tools, llm, agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION, verbose=True
+# )
 
 template = """
 You're a search agent, that helps to find answers to the questions based on the MLOPs Community Database.
@@ -27,16 +41,21 @@ Question: {question}
 
 @cl.langchain_factory(use_async=False)
 def main():
-    llm = OpenAI(temperature=0)
-    prompt = PromptTemplate(template=template, input_variables=["question"])
-    chain = LLMChain(llm=llm, prompt=prompt)
+    # chain = LLMChain(llm=llm, prompt=PromptTemplate.from_template(template))
+    # prompt = PromptTemplate(template=template, input_variables=["question"])
+
+    # Use RetrievalQAWithSourcesChain to return source of answer.
+    # TODO: Display sources
+    chain = RetrievalQAWithSourcesChain.from_chain_type(
+        llm, chain_type="stuff", retriever=weaviate_instance.as_retriever()
+    )
     return chain
 
 
-@cl.langchain_run
-async def run(input_str):
-    res = await cl.make_async(agent)(
-        input_str, callbacks=[cl.ChainlitCallbackHandler()]
-    )
-    print(res)
-    await cl.Message(content=res["output"]).send()
+# @cl.langchain_run
+# async def run(input_str):
+#    res = await cl.make_async(agent)(
+#        input_str, callbacks=[cl.ChainlitCallbackHandler()]
+#    )
+#    print(res)
+#    await cl.Message(content=res["output"]).send()
