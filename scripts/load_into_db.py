5cce8ae191f4331d4374386e8e95311a2a3ccef5
james.richardson <james.richardson@alexanderthamm.com>
2023-06-17 12:23:31 +0200

    feat: column rename to lowercase.

--- scripts/load_into_db.py
+++ scripts/load_into_db.py
@@ -4,9 +4,21 @@ import polars as pl
 
 REPO_ROOT = Path(__file__).parent.parent
 
+STANDARDIZED_SCHEMA = {
+    "__Source": "source",
+    "User_ID": "user_id",
+    "Channel_Name": "channel_name",
+    "Message_Timestamp": "message_timestamp",
+    "Thread_Timstamp": "thread_timstamp",
+    "Channel_ID": "channel_id",
+    "__Text": "text",
+}
+
 
 def main():
-    messages = pl.read_csv(REPO_ROOT / "data/messages.csv")
+    raw_messages = pl.read_csv(REPO_ROOT / "data/messages.csv")
+    messages = raw_messages.rename(STANDARDIZED_SCHEMA)
+
     print(messages)
 
 
