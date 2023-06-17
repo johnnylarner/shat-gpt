1d4303c29097d35d7c8b2318a85fe2183a1f8ede
james.richardson <james.richardson@alexanderthamm.com>
2023-06-17 16:46:54 +0200

    chore: remove dead code.

--- src/shat_gpt/app.py
+++ src/shat_gpt/app.py
@@ -1,12 +1,9 @@
 import os
 import chainlit as cl
 from dotenv import load_dotenv
-from langchain import PromptTemplate, OpenAI, LLMChain
-from langchain.agents import load_tools, initialize_agent, AgentType
+from langchain import OpenAI
 from langchain.chains import RetrievalQAWithSourcesChain
 from langchain.vectorstores import Weaviate
-from langchain.tools import BaseTool
-from llm_agent import CustomSearchTool
 import weaviate
 
 
@@ -28,10 +25,6 @@ weaviate_instance = Weaviate(
     client=client, index_name=CLASS_NAME, text_key="text", attributes=["source"]
 )
 llm = OpenAI(model_name="text-davinci-003", temperature=0.0)
-# tools = [CustomSearchTool()]
-# agent = initialize_agent(
-#     tools, llm, agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION, verbose=True
-# )
 
 template = """
 You're a search agent, that helps to find answers to the questions based on the MLOPs Community Database.
@@ -41,21 +34,9 @@ Question: {question}
 
 @cl.langchain_factory(use_async=False)
 def main():
-    # chain = LLMChain(llm=llm, prompt=PromptTemplate.from_template(template))
-    # prompt = PromptTemplate(template=template, input_variables=["question"])
-
     # Use RetrievalQAWithSourcesChain to return source of answer.
     # TODO: Display sources
     chain = RetrievalQAWithSourcesChain.from_chain_type(
         llm, chain_type="stuff", retriever=weaviate_instance.as_retriever()
     )
     return chain
-
-
-# @cl.langchain_run
-# async def run(input_str):
-#    res = await cl.make_async(agent)(
-#        input_str, callbacks=[cl.ChainlitCallbackHandler()]
-#    )
-#    print(res)
-#    await cl.Message(content=res["output"]).send()
