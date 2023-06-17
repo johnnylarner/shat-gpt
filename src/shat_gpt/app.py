8834b56184273e1310c2c2a68caddb776d7d386e
Jonas Kromolan <jonas.kromolan@alexanderthamm.com>
2023-06-17 12:12:48 +0200

    chainlit boilerplate

---
+++ src/shat_gpt/app.py
@@ -0,0 +1,17 @@
+from dotenv import load_dotenv
+from langchain import PromptTemplate, OpenAI, LLMChain
+import chainlit as cl
+
+load_dotenv()
+
+template = """Question: {question}
+
+Answer: Let's think step by step."""
+
+
+@cl.langchain_factory(use_async=True)
+def factory():
+    prompt = PromptTemplate(template=template, input_variables=["question"])
+    llm_chain = LLMChain(prompt=prompt, llm=OpenAI(temperature=0), verbose=True)
+
+    return llm_chain
--- src/shat_gpt/openai_client.py
+++
@@ -1,7 +0,0 @@
-from dotenv import load_dotenv
-import os
-import openai
-
-load_dotenv()
-openai.api_key = os.environ.get("OPENAI_API_KEY")
-
